07d9c75c1df2f495a8408bb4a7a69d2530aeeaf7
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-13 01:08:06 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -28,9 +28,7 @@ class Boat < ActiveRecord::Base
   end
 
   def self.with_three_classifications
-    a = joins(:classifications).group('boats.id').having('COUNT(*) = 3')
-    b = all.select { |b| b.classifications.size == 3 }
-    binding.pry
+    joins(:classifications).group('boats.id').having('COUNT(*) = 3')
   end
 
 end
