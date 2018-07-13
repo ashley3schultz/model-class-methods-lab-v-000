96fa28740cc3e132e73763ec0404f283f6a459b2
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-13 00:45:25 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -9,12 +9,21 @@ class Captain < ActiveRecord::Base
     includes(boats: :classifications).where(classifications: {name: 'Sailboat'}).uniq
   end
 
+<<<<<<< HEAD
   def self.motorboaters
+=======
+  def self.motorboats
+>>>>>>> 89b36431061d3eb9ff67772410911b1e24aaeab2
     includes(boats: :classifications).where(classifications: {name: 'Motorboat'}).uniq
   end
 
   def self.talented_seafarers
+<<<<<<< HEAD
     where("id IN (?)", self.sailors.pluck(:id) & self.motorboaters.pluck(:id))
+=======
+    includes(boats: :classifications).where(classifications: {name: 'Sailboat'},classifications: {name: 'Motorboat'}).all
+
+>>>>>>> 89b36431061d3eb9ff67772410911b1e24aaeab2
   end
 
   def self.non_sailors
